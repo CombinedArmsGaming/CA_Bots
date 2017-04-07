cbb911adfa219f6f3583dbc67dea81dc4e867161
Calum Brookes <calumbrookes@users.noreply.github.com>
2017-04-07 13:39:53 +0100

    Added comment header

--- cronjob.sh
+++ cronjob.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+#This script runs as a cronjob on the server to kill and restart the bot once a day.
+
 echo "Starting Colonel Sawyer..."
 sudo pkill -F /python/slackbot/pid.pid
 sudo python /python/slackbot/ops_control.py &
