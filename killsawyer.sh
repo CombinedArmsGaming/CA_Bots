721010060c77c3e9e6f1f2abbba0cb78eb962078
Calum Brookes <calumbrookes@users.noreply.github.com>
2017-04-07 13:42:35 +0100

    Added comment header

--- killsawyer.sh
+++ killsawyer.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+#A short script to retrieve the pid and kill the bots in the event of a manual service halt being necessary.
+
 echo "Killing Colonel Sawyer..."
 sudo pkill -F /python/slackbot/pid.pid
