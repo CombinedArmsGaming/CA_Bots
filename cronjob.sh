607f91d1346df0fad0e034b17305cfffcbb3e89f
Calum Brookes <calumbrookes@users.noreply.github.com>
2018-03-14 11:49:34 +0000

    Update cronjob.sh

--- cronjob.sh
+++ cronjob.sh
@@ -2,5 +2,5 @@
 echo "Starting Colonel Sawyer..."
 cd /slackbot/
 pkill -F /slackbot/pid.pid
-python /slackbot/main.py >> /slackbot/sawyerlog.log 2>&1 &
+python3 /slackbot/main.py >> /slackbot/sawyerlog.log 2>&1 &
 echo $! > /slackbot/pid.pid
